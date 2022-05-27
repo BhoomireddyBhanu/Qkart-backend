d9ea0332e1cc36ca45ddf88670a902dbd064a5ce
BhoomireddyBhanu <bhanureddyb2001@gmail.com>
2022-05-27 15:44:48 +0530

    github commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Register from "./components/Register";
+import ipConfig from "./ipConfig.json";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Products from "./components/Products";
+
+export const config = {
+  endpoint: `http://qkart-frontendbhoomireddybhanu.herokuapp.com/api/v1`,
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+          
+          <Route path="/register">
+            <Register />
+          // </Route>
+          // <Route path="/login">
+          //   <Login />
+          // </Route>
+          
+          // <Route exact path="/">
+          //   <Products />
+          // </Route>
+
+          //       <Route exact path='/' component={Products}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/checkout' component={Checkout}/>
+      <Route exact path='/thanks' component={Thanks}/>
+          
+        </Switch>
+    </div>
+  );
+}
+
+export default App;
